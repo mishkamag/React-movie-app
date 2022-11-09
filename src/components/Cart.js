f19dd0bcbd1c0361fe42ecb7152e6a3d10b55cf9
mishkamag <m93.maglaperidze@gmail.com>
2022-11-09 23:27:54 +0400

    fixed reviev color bugs

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,7 +4,7 @@ const Cart = (props) => {
   const getColor = (vote) => {
     if (vote >= 8) {
       return "green";
-    } else if (vote >= 5) {
+    } else if (vote >= 6) {
       return "orange";
     } else {
       return "red";
@@ -17,7 +17,7 @@ const Cart = (props) => {
         <img src={props.src} alt={props.alt} />
         <div className={classes.movieInfo}>
           <h3>{props.alt}</h3>
-          <span className={getColor(props.vote_average)}>
+          <span className={`colored ${getColor(props.vote_average)}`}>
             {props.vote_average}
           </span>
         </div>
