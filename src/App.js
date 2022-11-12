93f43f6edd8d354a18a27e0386fcc5c7c2eeb2b9
mishkamag <m93.maglaperidze@gmail.com>
2022-11-12 22:26:34 +0400

    fixed favMovies cards

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ function App() {
           />
         }
       />
+
       <Route
         path="/favorites"
         element={<FavMovies favoriteMovies={favoriteMovies} />}
--- src/components/pages/FavMovies.js
+++ src/components/pages/FavMovies.js
@@ -4,15 +4,21 @@ const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
 const FavMovies = (props) => {
   const { favoriteMovies } = props;
-  return favoriteMovies.map((movie) => (
-    <Cart
-      key={movie.id}
-      src={IMG_URL + movie.poster_path}
-      overview={movie.overview}
-      alt={movie.title}
-      vote_average={movie.vote_average}
-    />
-  ));
+
+  return (
+    <Card>
+      {favoriteMovies.map((movie) => (
+        <Cart
+          id={movie.id}
+          key={movie.id}
+          src={IMG_URL + movie.poster_path}
+          overview={movie.overview}
+          alt={movie.title}
+          vote_average={movie.vote_average}
+        />
+      ))}
+    </Card>
+  );
 };
 
 export default FavMovies;
