b199fb26a094107f4a3166476978fc022211d923
mishkamag <m93.maglaperidze@gmail.com>
2022-11-16 20:06:45 +0400

    log form with formik

--- src/components/authorization/Login.js
+++ src/components/authorization/Login.js
@@ -1,35 +1,69 @@
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 import classes from "./Login.module.css";
+import { useFormik } from "formik";
 
 const Login = () => {
   const navigate = useNavigate();
-  const loginSubmitHandler = (event) => {
-    event.preventDefault();
-    navigate("/");
-  };
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      date: "",
+    },
+    onSubmit: (values) => {
+      navigate("/");
+      console.log(values);
+    },
+  });
+
+  // const loginSubmitHandler = (event) => {
+  //   event.preventDefault();
+  //   navigate("/");
+  // };
 
   return (
     <Fragment>
       <div className={classes.site}>ADJARANET</div>
       <section className={classes.login}>
-        <form onSubmit={loginSubmitHandler}>
+        <form onSubmit={formik.handleSubmit}>
           <div className={classes.name}>
             <label htmlFor="name">Name</label>
-            <input type="text" id="name" name="name" />
+            <input
+              type="text"
+              id="name"
+              name="name"
+              onChange={formik.handleChange}
+              value={formik.values.name}
+            />
           </div>
 
           <div className={classes.email}>
             <label htmlFor="email">E-mail</label>
-            <input type="email" id="email" name="email" />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              onChange={formik.handleChange}
+              value={formik.values.email}
+            />
           </div>
 
           <div className={classes.date}>
             <label thmlfor="date">Birth Date </label>
-            <input type="date" id="date" name="date" />
+            <input
+              type="date"
+              id="date"
+              name="date"
+              onChange={formik.handleChange}
+              value={formik.values.date}
+            />
           </div>
 
-          <button className={classes.button}>Log In</button>
+          <button type="submit" className={classes.button}>
+            Log In
+          </button>
         </form>
       </section>
     </Fragment>
